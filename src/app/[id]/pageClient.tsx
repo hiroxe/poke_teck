102b3ae64ba2b656f26e914d4f21bbf84d56dd1a
ismaelgarciataclia <147412092+ismaelgarciataclia@users.noreply.github.com>
2025-11-05 00:28:18 +0100

    spinner

--- src/app/[id]/pageClient.tsx
+++ src/app/[id]/pageClient.tsx
@@ -1,22 +1,35 @@
 "use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import type { IPokemon } from "~/types/pokemon";
 import { EvolutionList } from "../_components/evolutionList";
 import { useRouter } from "next/navigation";
 
 function PokemonClientDetail({ pokemon }: { pokemon: IPokemon }) {
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = () => {
+    setLoading(true);
+    router.push("/");
+  };
 
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center gap-5 p-8">
+    <div className="flex h-full w-full flex-col items-center justify-center gap-5 p-3 md:p-8">
       <div
         className="pointer-events-none fixed inset-0 -z-10 bg-cover bg-center opacity-20 grayscale-[50%] saturate-75"
         style={{ backgroundImage: "url('/background.png')" }}
       />
       <div className="flex flex-col gap-5 bg-white p-5">
-        <div className="cursor-pointer" onClick={() => router.push(`/`)}>
-          BACK
+        <div
+          className="flex cursor-pointer items-center gap-2"
+          onClick={handleClick}
+        >
+          {loading ? (
+            <span className="inline-block h-5 w-5 animate-spin rounded-full border-4 border-gray-400 border-t-transparent"></span>
+          ) : (
+            "BACK"
+          )}
         </div>
         <div className="flex h-fit flex-col gap-3 md:h-[250px] md:flex-row">
           <div className="flex flex-col items-center">
