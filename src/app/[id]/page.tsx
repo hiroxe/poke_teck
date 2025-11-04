d2c84ea7ae982aaacf172cf3716f88db96063c7c
ismaelgarciataclia <147412092+ismaelgarciataclia@users.noreply.github.com>
2025-11-05 00:17:17 +0100

    error fix

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,11 +1,26 @@
+import { notFound } from "next/navigation";
 import { api, HydrateClient } from "~/trpc/server";
 import PokemonClientDetail from "./pageClient";
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const pokemon = await api.pokemon.getPokemonById(params.id);
-  return (
-    <HydrateClient>
-      <PokemonClientDetail pokemon={pokemon} />
-    </HydrateClient>
-  );
+type Props = { params: { id: string } | Promise<{ id: string }> };
+
+export default async function Page({ params }: Props) {
+  const { id } = await params;
+
+  try {
+    const pokemon = await api.pokemon.getPokemonById(id);
+
+    if (!pokemon) {
+      return notFound();
+    }
+
+    return (
+      <HydrateClient>
+        <PokemonClientDetail pokemon={pokemon} />
+      </HydrateClient>
+    );
+  } catch (error) {
+    console.error("Error fetching pokemon by id:", error);
+    return notFound();
+  }
 }
